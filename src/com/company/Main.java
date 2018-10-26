ecba31722cce56f4df0fbc90734ba85bb18e75c5
Liyto1 <lev.liv2@gmai.com>
2018-10-26 18:25:51 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+
+///////////////////////lab5zad1///////////
+/*public class Main {
+        public static int n=0;
+        public static void main(String[] args) {
+            fclass3 fc3=new fclass3();
+            fclass2 fc2=new fclass2();
+            fc2.method1();
+            fc3.method2();
+        }}
+    class fclass2 extends fclass3{
+        public void method1(){
+            m=5;
+            System.out.println(m);
+      }}
+    class fclass3 extends Main{
+        public static int m;
+        public void method2(){
+            n=20;
+            System.out.println(n);
+        }}*/
+///////////////////////lab5zad2///////////
+/*class Main {
+        protected int x=5;
+        public static void main(String[] args) {
+            fclass2 fc =new fclass2();
+            System.out.print(f.method());
+        }
+    }
+    class fclass2 extends Main{
+        public int method(){
+            int y;
+            int z=2;
+            y=x+z;
+            return y;
+        }
+    }*/
